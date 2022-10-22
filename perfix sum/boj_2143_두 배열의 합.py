4aa0a6f5d9749ac1a274639a7cbca395c9b3dc84
tree.jhk <tree.jhk@gmail>
2022-10-22 19:29:30 +0900

    코드 수정

--- perfix sum/boj_2143_두 배열의 합.py
+++ perfix sum/boj_2143_두 배열의 합.py
@@ -1,8 +1,16 @@
+import imp
 import sys
 input = sys.stdin.readline
+from itertools import accumulate as acc
 
 t = int(input().strip())
 n = int(input().strip())
 a = list(map(int,input().split()))
 m = int(input().strip())
 b = list(map(int,input().split()))
+
+a_acc = [0] + list(acc(a))
+b_acc = [0] + list(acc(b))
+
+print(a_acc)
+print(b_acc)
