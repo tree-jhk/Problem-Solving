2b23d30a427e83e78ce2aa636b7b5ce045f7ba73
egoing <tree.jhk@gmail>
2022-10-22 18:54:07 +0900

    코드 수정

--- perfix sum/boj_2143_두 배열의 합.py
+++ perfix sum/boj_2143_두 배열의 합.py
@@ -1,2 +1,8 @@
 import sys
 input = sys.stdin.readline
+
+t = int(input().strip())
+n = int(input().strip())
+a = list(map(int,input().split()))
+m = int(input().strip())
+b = list(map(int,input().split()))
