595fbb4829bdb1e3d96c9b33b20adfbf0b4b6bfd
tree.jhk <tree.jhk@gmail>
2022-10-23 15:28:27 +0900

    한 문제 또 품

--- perfix sum/boj_2143_두 배열의 합.py
+++ perfix sum/boj_2143_두 배열의 합.py
@@ -1,16 +1,21 @@
-import imp
 import sys
 input = sys.stdin.readline
 from itertools import accumulate as acc
+from itertools import combinations as combi
+import bisect as bs
 
 t = int(input().strip())
 n = int(input().strip())
 a = list(map(int,input().split()))
 m = int(input().strip())
 b = list(map(int,input().split()))
+answer = 0
 
-a_acc = [0] + list(acc(a))
-b_acc = [0] + list(acc(b))
+a_ = sorted([cb[1] - cb[0] for cb in combi([0] + list(acc(a)), 2)])
+b_ = sorted([cb[1] - cb[0] for cb in combi([0] + list(acc(b)), 2)])
+for i in range(len(a_)):
+    l = bs.bisect_left(b_, t - a_[i])
+    r = bs.bisect_right(b_, t - a_[i])
+    answer += (r - l)
 
-print(a_acc)
-print(b_acc)
+print(answer)
