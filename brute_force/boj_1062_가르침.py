2bf1e090bb0f443ed081fcd0b46cee93b3e1755e
tree.jhk <tree.jhk@gmail>
2022-10-27 10:00:01 +0900

    커밋

--- brute_force/boj_1062_가르침.py
+++ brute_force/boj_1062_가르침.py
@@ -1,8 +1,11 @@
 import sys
 input = sys.stdin.readline
+from itertools import combinations as combi
 
 n, k = map(int, input().split())
 words = [input().strip() for _ in range(n)]
+
+answer = 0
 
 k -= 5
 if k < 0:
@@ -11,6 +14,22 @@ if k < 0:
 for i in range(n):
     words[i] = (words[i][4:] + words[i][:-4])
     tmp = set(words[i]) - set('antic')
-    words[i] = tmp
+    if len(tmp) == 0:
+        answer += 1
+    else:
+        words[i] = tmp
 words = list(map(''.join, words))
-print(words)
+candi = set(''.join(words))
+
+# print(words)
+for cb in combi(candi,k):
+    cnt = 0
+    letters = ''.join(cb)
+    for word in words:
+        for l in word:
+            if l not in letters:
+                break
+        else:
+            cnt += 1
+    answer = max(answer, cnt)
+print(answer)
